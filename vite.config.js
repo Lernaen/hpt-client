57279646e95360d2a443e5dbe9049d09a93cc14e
KhokharJunaid <muhammadjunaidali67@gmail.com>
2025-02-19 15:49:00 +0500

    feat: deployment setup

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,14 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  // Load environment variables based on mode (e.g., development, production)
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_BACKEND_BASE_URL, // Use the loaded env variable
-          changeOrigin: true,
-          secure: false,
-        },
-      },
+    define: {
+      'import.meta.env.VITE_BACKEND_BASE_URL': JSON.stringify(
+        env.VITE_BACKEND_BASE_URL,
+      ),
     },
   }
 })
