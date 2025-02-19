93249b28aa00f82e12e3558c9416d847af15912c
KhokharJunaid <muhammadjunaidali67@gmail.com>
2025-02-19 16:14:28 +0500

    feat: some eslint and url setting

--- src/apis/jobs.ts
+++ src/apis/jobs.ts
@@ -4,6 +4,7 @@ const BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL
 
 export async function getAllJobs() {
   const res = await request.get(`${BASE_URL}/api/v1/jobs`)
+  console.log('resBody', res.body)
   return res.body as Job[]
 }
 
@@ -15,5 +16,5 @@ export async function getJobById(id: number) {
 export async function addJob(newjob: NewJob) {
   const res = await request.post(`${BASE_URL}/api/v1/jobs`).send(newjob)
   console.log(res.statusCode)
-  return
+  return res.body as Job
 }
